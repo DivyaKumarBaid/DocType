ffe3f0421635c0f77ae32ed09cf9fe7b254c6c79
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2023-01-29 23:47:06 +0530

    logo

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
-import Header from "../src/components/Header";
 import FileUpload from "../src/components/FileUpload";
 import { CanvasProvider } from "../src/context/CanvasContext";
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <CanvasProvider>
+      <Head>
+        <title>!DocType</title>
+        <meta
+          name="description"
+          content="Edit your pdf online in an instant and download instantly."
+        />
+        <link rel="icon" href="./logo.png" />
+      </Head>
       <FileUpload />
     </CanvasProvider>
   );
