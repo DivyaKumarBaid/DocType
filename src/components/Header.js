de562fcc19c7a282d80b05bdd2cd66c1222c0c5c
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2023-01-29 17:15:54 +0530

    base commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="md:flex md:items-center md:justify-between px-6 py-4 shadow-md">
+    <div className="md:flex md:items-center md:justify-between px-6 py-2 shadow-md">
       <a href="/">
-        <img src="./navLogo.png" className="ml-8" width={140} alt="" /></a>
+        <img src="./navLogo.png" className="ml-8" width={120} alt="" /></a>
       <div className="flex justify-between gap-10 items-center mr-16">
         <a href="https://github.com/VishalChoubey1019/Njack_Hack-It-Out-edgelamp"><AiFillGithub className="text-4xl" /></a>
         <a href="/guide"> <TiInfoLarge className="text-3xl cursor-pointer" /></a>
