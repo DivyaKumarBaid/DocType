2eec883443af10784f02723431fc0b6ae9c2c52c
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2023-01-29 23:50:26 +0530

    link bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import { AiFillGithub } from "react-icons/ai";
 import { TiInfoLarge } from "react-icons/ti";
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <div className="md:flex md:items-center md:justify-between px-6 py-2 shadow-md">
-      <a href="/">
-        <img src="./navLogo.png" className="ml-8" width={120} alt="" /></a>
+      <Link href="/">
+        <img src="./navLogo.png" className="ml-8" width={120} alt="" /></Link>
       <div className="flex justify-between gap-10 items-center mr-16">
-        <a href="https://github.com/VishalChoubey1019/Njack_Hack-It-Out-edgelamp"><AiFillGithub className="text-4xl" /></a>
-        <a href="/guide"> <TiInfoLarge className="text-3xl cursor-pointer" /></a>
+        <Link href="https://github.com/VishalChoubey1019/Njack_Hack-It-Out-edgelamp"><AiFillGithub className="text-4xl" /></Link>
+        <Link href="/guide"> <TiInfoLarge className="text-3xl cursor-pointer" /></Link>
       </div>
     </div>
   )
