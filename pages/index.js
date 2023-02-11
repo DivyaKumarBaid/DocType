cddea81dbb4d704a769d17315726cc4addf5b750
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2023-02-11 18:18:14 +0530

    added some head tag content

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,51 @@ export default function Home() {
     <div>
       <Head>
         <title>!DocType</title>
-        <meta
-          name="description"
-          content="Edit your pdf online in an instant and download instantly."
-        />
-        <link rel="icon" href="./logo.png" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="description" content="Transform the way you manage and edit your documents with Doctype, the innovative document management software. Sign up today!" />
+        <meta name="keywords" content="document management software, document editing software, document editing, document management, doctype software, doctype editing, PDF editor, online editor, document editing, edit PDF, online PDF editing" />
+        <link rel="icon" href="./logo.png" type="image/x-icon" />
+        <link rel="shortcut icon" href="./logo.png" type="image/x-icon" />
+        <title>Doctype - Revolutionize Your Document Management</title>
+
+        {/* <!-- Structured Data --> */}
+        <script type="application/ld+json">
+          {
+            {
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "url": "https://doctype.software/",
+              "name": "Doctype",
+              "description": "Transform the way you manage and edit your pdf with Doctype, the innovative document management software.",
+              "potentialAction": {
+                "@type": "SearchAction",
+                "target": "https://doctype.software/search?q={search_term_string}",
+                "query-input": "required name=search_term_string"
+              }
+            }
+          }
+        </script>
+
+        {/* <!-- Open Graph --> */}
+        <meta property="og:title" content="Doctype - Revolutionize Your Document Management" />
+        <meta property="og:description" content="Transform the way you manage and edit your documents with Doctype, the innovative document management software." />
+        <meta property="og:image" content="https://i.postimg.cc/xT6B0t4y/Asset-49.png" />
+        <meta property="og:url" content="https://www.doctype.software/" />
+        <meta property="og:type" content="website" />
+
+        {/* <!-- Twitter --> */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@doctype_software" />
+        <meta name="twitter:title" content="Doctype - Revolutionize Your Document Management" />
+        <meta name="twitter:description" content="Transform the way you manage and edit your documents with Doctype, the innovative document management software." />
+        <meta name="twitter:image" content="https://i.postimg.cc/xT6B0t4y/Asset-49.png" />
+
+        {/* <!-- Alternate Languages --> */}
+        <link rel="alternate" href="https://www.doctype.software/" hreflang="en-US" />
+        <link rel="alternate" href="https://www.doctype.software/" hreflang="fr-FR" />
+
       </Head>
       <FileUpload />
     </div>
