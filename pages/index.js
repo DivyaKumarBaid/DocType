3b90c13d03469b0d1d4ceaab52cfae592292bd5e
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2023-02-11 18:56:50 +0530

    keywords

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta name="description" content="Transform the way you manage and edit your documents with Doctype, the innovative document management software. Sign up today!" />
-        <meta name="keywords" content="document management software, document editing software, document editing, document management, doctype software, doctype editing, PDF editor, online editor, document editing, edit PDF, online PDF editing" />
+        <meta name="keywords" content="document management software, document editing software, document editing, document management, doctype software, doctype editing, PDF editor, online editor, document editing, edit PDF, online PDF editing, Online PDF editor, edit PDF online, PDF editing software, cloud-based PDF editor, free PDF editor, PDF editor for collaboration, PDF editor with OCR, online PDF annotation tool, PDF form filler, PDF converter" />
         <link rel="icon" href="./logo.png" type="image/x-icon" />
         <link rel="shortcut icon" href="./logo.png" type="image/x-icon" />
         <title>Doctype - Revolutionize Your Document Management</title>
